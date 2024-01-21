8807c7b28703ccaae64e0f1ce5384f66d2f256bd
tyleroni <tjshin@ucsd.edu>
2024-01-21 03:08:42 -0800

    f

--- src/index.js
+++ src/index.js
@@ -7,15 +7,13 @@ import { BrowserRouter } from 'react-router-dom';
 
 
 
-//const root = ReactDOM.createRoot(document.getElementById("root"));
-//root.render(
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode >
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
