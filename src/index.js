5ca8313853f7b857ddf649c261ef8b682caa6456
tyleroni <tjshin@ucsd.edu>
2024-01-21 03:32:22 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-
+import { createRoot } from "react-dom/client";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
