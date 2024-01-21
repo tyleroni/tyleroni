128e8ebd8566e9fa54d4ceca00f32c00593cbf4a
tyleroni <tjshin@ucsd.edu>
2024-01-21 10:54:37 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ import { createRoot } from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  //<React.StrictMode >
+  <React.StrictMode >
     <BrowserRouter basename='/'>
       <App />
     </BrowserRouter>
-  //</React.StrictMode>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
