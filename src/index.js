bf50ecc903bd38845f621898c349934ec12a5c59
tyleroni <tjshin@ucsd.edu>
2024-01-21 18:57:51 -0800

    dafjk

--- src/index.js
+++ src/index.js
@@ -3,18 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from "react-dom";
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode >
-    <HashRouter basename = '/tyleroni'>
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </HashRouter>
-  </React.StrictMode>
-);
+    </BrowserRouter>
+    ,
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
